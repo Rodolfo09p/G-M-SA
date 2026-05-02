e0adb1b691b3ee43df7eb8daa9c91f7daab82c41
Rodolfo <rodolfoponce119@gmail.com>
2026-05-02 14:18:47 -0600

    feat: Revamp navigation configuration with new groups and items for improved organization

--- src/configs/navigationConfig.ts
+++ src/configs/navigationConfig.ts
@@ -8,32 +8,113 @@ i18n.addResourceBundle('en', 'navigation', en);
 i18n.addResourceBundle('tr', 'navigation', tr);
 i18n.addResourceBundle('ar', 'navigation', ar);
 
-/**
- * The navigationConfig object is an array of navigation items for the Fuse application.
- */
 const navigationConfig: FuseNavItemType[] = [
 	{
-		id: 'dashboard-component',
-		title: 'Dashboard',
-		type: 'item',
-		icon: 'lucide:layout-dashboard',
-		url: 'dashboard'
+		id: 'panel-control-group',
+		title: 'PANEL DE CONTROL',
+		type: 'group',
+		icon: 'heroicons-outline:home',
+		children: [
+			{
+				id: 'dashboard-component',
+				title: 'Dashboard',
+				type: 'item',
+				icon: 'heroicons-outline:chart-pie',
+				url: 'dashboard'
+			}
+		]
 	},
 	{
-		id: 'customers-component',
-		title: 'Clientes',
-		type: 'item',
-		icon: 'lucide:users',
-		url: 'customers'
+		id: 'gestion-operativa',
+		title: 'GESTIÓN OPERATIVA',
+		type: 'group',
+		icon: 'heroicons-outline:briefcase',
+		children: [
+			{
+				id: 'customers-component',
+				title: 'Clientes',
+				type: 'item',
+				icon: 'heroicons-outline:user-group',
+				url: 'customers'
+			},
+			{
+				id: 'policies-component',
+				title: 'Pólizas',
+				type: 'item',
+				icon: 'heroicons-outline:document-duplicate',
+				url: 'policies'
+			},
+			{
+				id: 'claims-component',
+				title: 'Reclamos (Siniestros)',
+				type: 'item',
+				icon: 'heroicons-outline:exclamation-circle',
+				url: 'claims'
+			}
+		]
 	},
 	{
-		id: 'policies-component',
-		title: 'Polizas',
-		type: 'item',
-		icon: 'lucide:file-text',
-		url: 'policies'
+		id: 'modulo-busqueda',
+		title: 'BÚSQUEDAS',
+		type: 'group',
+		icon: 'heroicons-outline:magnifying-glass',
+		children: [
+			{
+				id: 'search-advanced',
+				title: 'Búsqueda Avanzada',
+				type: 'item',
+				icon: 'heroicons-outline:magnifying-glass',
+				// url: 'search/advanced'
+				url: ''
+			}
+		]
 	},
-
+	{
+		id: 'modulo-reportes',
+		title: 'REPORTES',
+		type: 'group',
+		icon: 'heroicons-outline:presentation-chart-line',
+		children: [
+			{
+				id: 'reports-collections',
+				title: 'Cobranza',
+				type: 'item',
+				icon: 'heroicons-outline:banknotes',
+				url: ''
+			},
+			{
+				id: 'reports-renewals',
+				title: 'Próximas Renovaciones',
+				type: 'item',
+				icon: 'heroicons-outline:arrow-path',
+				// url: 'reports/renewals'
+				url: ''
+			}
+		]
+	},
+	{
+		id: 'administracion-sistema',
+		title: 'ADMINISTRACIÓN',
+		type: 'group',
+		icon: 'heroicons-outline:cog-6-tooth',
+		children: [
+			{
+				id: 'catalogos-component',
+				title: 'Catálogos',
+				type: 'item',
+				icon: 'heroicons-outline:list-bullet',
+				url: ''
+			},
+			{
+				id: 'users-component',
+				title: 'Usuarios y Permisos',
+				type: 'item',
+				icon: 'heroicons-outline:user-plus',
+				// url: 'settings/users'
+				url: ''
+			},
+		]
+	}
 ];
 
 export default navigationConfig;
